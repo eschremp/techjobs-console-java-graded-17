fa7c5091a389a0fb5acf7a8ae8811dc2fab3f238
eschremp <eschremp93@gmail.com>
2024-01-04 18:58:34 -0600

    passed local test

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -5,10 +5,7 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -75,7 +72,7 @@ public class JobData {
 
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+            if (aValue.toLowerCase().contains(value.toLowerCase())) {
                 jobs.add(row);
             }
         }
@@ -95,7 +92,15 @@ public class JobData {
         loadData();
 
         // TODO - implement this method
-        return null;
+        ArrayList<HashMap<String, String>> resultsList = new ArrayList<>();
+        for (HashMap<String, String> i : allJobs) {
+            String valueArr = i.values().toString().toLowerCase();
+            String testValue = value.toLowerCase();
+            if (valueArr.contains(testValue)){
+                resultsList.add(i);
+            }
+        }
+        return resultsList;
     }
 
     /**
